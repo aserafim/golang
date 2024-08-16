00845346550471ebc08159c5210b5d403ecd231d
aserafim <correia.alefe@gmail.com>
2024-08-16 18:38:18 -0300

    :sparkles: feat: Importando pacotes externos

--- 001 - Pacotes/main.go
+++ 001 - Pacotes/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"pacotes/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Testando funcionamento do módulo.")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("teste1@teste2.com")
+	fmt.Println(erro)
 
 }
