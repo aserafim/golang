34f65c02efffdcfae9026e8f9baf06b30950c505
aserafim <correia.alefe@gmail.com>
2024-08-17 09:31:33 -0300

    :sparkles: feat: Entendendo operadores

---
+++ 05 - Operadores/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//GoLang não permite cálculos
+	//com variáveis de tipos diferentes
+	//O código abaixo não funciona
+	//var num1 int16 = 10
+	//var num2 int32 = 15
+	//soma := num1 + num2
+
+	//unários
+	num := 10
+	num++
+	num += 10
+	fmt.Println(num)
+
+	//ternario
+	
+
+}
